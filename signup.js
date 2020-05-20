126220462d4e8956a6330ce23aacb63de165d909
jurasw <57913219+jurasw@users.noreply.github.com>
2020-05-20 23:11:12 +0200

    Add files via upload

--- signup.js
+++ signup.js
@@ -3,15 +3,43 @@ function signUp(){
     var email = document.getElementById("email");
     var password = document.getElementById("password");
     var cpass = document.getElementById("cpass");
+    var code = document.getElementById("code");
     if(password.value === cpass.value){
-  auth.createUserWithEmailAndPassword(email.value, password.value).then(cred => {
-      console.log(cred.user)
-  })
- 
   
+  
+  if(code.value === "adminxxx2020"){
+  auth.createUserWithEmailAndPassword(email.value, password.value).then(cred => {
+      return db.collection('users').doc(cred.user.uid).ser({
+          admin: true
+      })
+  })  
   alert("Signed Up");
+  window.location.href = "task.html";}
 
-     window.location.href = "task.html";
+  if(code.value === "manager2020"){
+    auth.createUserWithEmailAndPassword(email.value, password.value).then(cred => {
+        return db.collection('users').doc(cred.user.uid).ser({
+            manager: true,
+            admin: false
+        })
+    })  
+    alert("Signed Up");
+    window.location.href = "task.html";}
+    
+    
+  if(code.value === "stazxxx2020"){
+        auth.createUserWithEmailAndPassword(email.value, password.value).then(cred => {
+            return db.collection('users').doc(cred.user.uid).ser({
+                manager: false,
+                admin: false,
+                stazysta: ture
+            })
+        })  
+        alert("Signed Up");
+        window.location.href = "task.html";}
+
+
+     
     }
     else{
         alert("passwords don't match");
