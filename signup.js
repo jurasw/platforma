2cf14e3c053688181d8bd4bec200d4456e8d5b3f
jurasw <57913219+jurasw@users.noreply.github.com>
2020-05-20 23:16:17 +0200

    Add files via upload

--- signup.js
+++ signup.js
@@ -37,7 +37,9 @@ function signUp(){
         })  
         alert("Signed Up");
         window.location.href = "task.html";}
-
+else{
+    alert("wrong company code");
+}
 
      
     }
@@ -62,3 +64,23 @@ function signUp(){
     
    }
     
+   const inputs = document.querySelectorAll(".input");
+
+
+   function addcl(){
+       let parent = this.parentNode.parentNode;
+       parent.classList.add("focus");
+   }
+   
+   function remcl(){
+       let parent = this.parentNode.parentNode;
+       if(this.value == ""){
+           parent.classList.remove("focus");
+       }
+   }
+   
+   
+   inputs.forEach(input => {
+       input.addEventListener("focus", addcl);
+       input.addEventListener("blur", remcl);
+   });
