4bb816f778ecdf9e129ce0b1bf3ae07915448e59
jurasw <57913219+jurasw@users.noreply.github.com>
2020-05-21 15:02:55 +0200

    Add files via upload

--- profile.js
+++ profile.js
@@ -6,38 +6,33 @@ function signOut(){
     alert("Signed Out");
     window.location.href = "index.html";
    }
-
+   
+   firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // User logged in already or has just logged in.
+      console.log(user.uid);
+    } else {
+      // User not logged in or has just logged out.
+    }
+  });
   
   object.onload = function profilinfo(){
 
-    document.getElementById("uname").innerHTML  = "Jurek";
-    document.getElementById("ulastname").innerHTML  = "Wiśniewski";
-}
+    firebase.database().ref('users/' + user.uid).once('value',   function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var childKey = childSnapshot.key;
+        var childData = childSnapshot.val();
+        info_array.push(Object.values(childData));
+
+       
+        document.getElementById("uname").innerHTML  =  task_date = info_array[i][5];
+        document.getElementById("ulastname").innerHTML = task_title = task_array[i][6];
+         
+      });
+    }); }
+    
 
 
-
-const themeMap = {
-    dark: "light",
-    light: "solar",
-    solar: "dark"
-  };
-  
-  const theme = localStorage.getItem('theme')
-    || (tmp = Object.keys(themeMap)[0],
-        localStorage.setItem('theme', tmp),
-        tmp);
-  const bodyClass = document.body.classList;
-  bodyClass.add(theme);
-  
-  function toggleTheme() {
-    const current = localStorage.getItem('theme');
-    const next = themeMap[current];
-  
-    bodyClass.replace(current, next);
-    localStorage.setItem('theme', next);
-  }
-  
-  document.getElementById('themeButton').onclick = toggleTheme;
 
 
 
