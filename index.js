4ee8ca96dccdb3c106ad672ade24e9bfa48283d4
jurasw <57913219+jurasw@users.noreply.github.com>
2020-05-22 11:11:01 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -26,18 +26,11 @@ function signUp(){
     
    }
    
-   var input = document.getElementById("myInput");
-
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("signUp").click();
-  }
-});
+   $("#pass").keypress(function(event) { 
+    if (event.keyCode === 13) { 
+        signIn(); 
+    } 
+}); 
    
    auth.onAuthStateChanged(function(user){
     
