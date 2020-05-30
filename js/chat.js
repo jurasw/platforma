3b8372efe2d96e1d9b3b2ceb4a474f694379cb03
jurasw <57913219+jurasw@users.noreply.github.com>
2020-05-30 18:43:20 +0200

    Add files via upload

--- js/chat.js
+++ js/chat.js
@@ -1,31 +1,29 @@
-var messages = ('.messages-content'),
+var $messages = $('.messages-content'),
     d, h, m,
     i = 0;
 
 var myName = "";
 
-function idk() {
-
-  //(messages).mCustomScrollbar();
+$(window).load(function() {
+  myName = prompt("Enter your name");
+  $messages.mCustomScrollbar();
 
   firebase.database().ref("messages").on("child_added", function (snapshot) {
     if (snapshot.val().sender == myName) {
-     // ('<div class="message message-personal"><figure class="avatar"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpdX6tPX96Zk00S47LcCYAdoFK8INeCElPeJrVDrh8phAGqUZP_g" /></figure><div id="message-' + snapshot.key + '">' + snapshot.val().message + '<button class="btn-delete" data-id="' + snapshot.key + '" onclick="deleteMessage(this);">Delete</button></div></div>').appendTo(('.mCSB_container')).addClass('new');
-      ('.message-input').val(null);
+      $('<div class="message message-personal"><figure class="avatar"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpdX6tPX96Zk00S47LcCYAdoFK8INeCElPeJrVDrh8phAGqUZP_g" /></figure><div id="message-' + snapshot.key + '">' + snapshot.val().message + '<button class="btn-delete" data-id="' + snapshot.key + '" onclick="deleteMessage(this);">Delete</button></div></div>').appendTo($('.mCSB_container')).addClass('new');
+      $('.message-input').val(null);
     } else {
-      //('<div class="message new"><figure class="avatar"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpdX6tPX96Zk00S47LcCYAdoFK8INeCElPeJrVDrh8phAGqUZP_g" /></figure><div id="message-' + snapshot.key + '">' + snapshot.val().sender + ': ' + snapshot.val().message + '</div></div>').appendTo(('.mCSB_container')).addClass('new');
+      $('<div class="message new"><figure class="avatar"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpdX6tPX96Zk00S47LcCYAdoFK8INeCElPeJrVDrh8phAGqUZP_g" /></figure><div id="message-' + snapshot.key + '">' + snapshot.val().sender + ': ' + snapshot.val().message + '</div></div>').appendTo($('.mCSB_container')).addClass('new');
     }
     
     setDate();
     updateScrollbar();
   });
 
-}
-
-window.addEventListener('load', idk());
+});
 
 function updateScrollbar() {
-  messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+  $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
     scrollInertia: 10,
     timeout: 0
   });
@@ -35,33 +33,26 @@ function setDate(){
   d = new Date()
   if (m != d.getMinutes()) {
     m = d.getMinutes();
-    ('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo(('.message:last'));
+    $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
   }
 }
 
 function insertMessage() {
-  msg = ('.message-input').value;
-  if (msg == '') {
+  msg = $('.message-input').val();
+  if ($.trim(msg) == '') {
     return false;
   }
 
   sendMessage();
 }
 
+$('.message-submit').click(function() {
+  insertMessage();
+});
 
-
-message.setAttribute('onclick', insertMessage());
-
-jQuery(window).on('keydown', function(e) {
+$(window).on('keydown', function(e) {
   if (e.which == 13) {
     insertMessage();
     return false;
   }
 });
-
-function signOut(){
-    
-  auth.signOut();
- // alert("Signed Out");
-  window.location.href = "https://jurasw.github.io/platforma/";
- }
